ea42a5dcaabfb7d80af0efa6a9d7f15e88d93e75
Trung <mitvan990@gmail.com>
2025-08-31 11:46:00 +0700

    first commit

---
+++ js/verification.js
@@ -0,0 +1,15 @@
+$("#login-button").click(function (event) {
+  let userName = document.getElementById("userName").value;
+  let pwd = document.getElementById("pwd").value;
+  if (userName == "Ngọc Linh" && pwd == "3108") {
+    $("#h").text("wellcome！");
+    event.preventDefault();
+    $("form").fadeOut(500);
+    $(".wrapper").addClass("form-success");
+    setTimeout(function () {
+      location.href = "BirthdayCake.html";
+    }, 4000);
+  } else {
+    alert("Tên người dùng hoặc mật khẩu không đúng！");
+  }
+});
