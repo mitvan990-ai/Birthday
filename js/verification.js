54d8edb3ecedf88fd37f0f4ab9d5cf171eb1b95b
mitvan990-ai <mitvan990@gmail.com>
2025-10-01 00:03:57 +0700

    Update verification.js

--- js/verification.js
+++ js/verification.js
@@ -1,7 +1,7 @@
 $("#login-button").click(function (event) {
   let userName = document.getElementById("userName").value;
   let pwd = document.getElementById("pwd").value;
-  if (userName == "Ngọc Linh" && pwd == "3108") {
+  if (userName == "Ngọc Thanh" && pwd == "0110") {
     $("#h").text("wellcome！");
     event.preventDefault();
     $("form").fadeOut(500);
